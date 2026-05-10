5d905d5039b0c75e5470ce13b977057c1501b151
amandasguedes <amanda.guedes@aluno.ce.gov.br>
2026-05-09 21:29:35 -0300

    Update q11.java

--- q11.java
+++ q11.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class q11 {
     public static void main(String[] args) {
         Locale.setDefault(new Locale("pt", "BR"));
-        Scanner sc = new Scanner(System.in);
-        System.out.print("digite uma nota: ");
-        double nota1 = sc.nextDouble();
-        System.out.print("digite a segunda nota: ");
-        double nota2 = sc.nextDouble();
-        double notaFinal = nota1 + nota2;
-        System.out.printf("nota final = %.1f%n", notaFinal);
-        if (notaFinal<60.0) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("digite uma nota:");
+        double n1=sc.nextDouble();
+        System.out.print("digite a segunda nota:");
+        double n2=sc.nextDouble();
+        double nf=n1+n2;
+        System.out.printf("nota final = %.1f%n", nf);
+        if (nf<60.0) {
             System.out.println("REPROVADO");
         }
         sc.close();
