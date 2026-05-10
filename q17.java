1a36a1592a7147b99796bc6f9a7ab499c1c02880
amandasguedes <amanda.guedes@aluno.ce.gov.br>
2026-05-09 21:34:49 -0300

    Update q17.java

--- q17.java
+++ q17.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 public class q17{
     public static void main(String[] args) {
         Locale.setDefault(new Locale("pt", "BR"));
-        Scanner sc = new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
         double d1, d2, d3, maior;
         System.out.println("digite as tres distancias:");
         d1 = sc.nextDouble();
         d2 = sc.nextDouble();
         d3 = sc.nextDouble();
         if (d1 > d2 && d1 > d3) {
-            maior = d1;
+            maior=d1;
         } 
-        else if (d2 > d3) {
-            maior = d2;
+        else if (d2>d3) {
+            maior=d2;
         } 
         else {
-            maior = d3;
+            maior=d3;
         }
         System.out.printf("MAIOR DISTANCIA = %.2f%n", maior);
         sc.close();
