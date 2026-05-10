ca7a2850879216032e7296bb061700042bf38267
amandasguedes <amanda.guedes@aluno.ce.gov.br>
2026-05-09 21:26:35 -0300

    Update q9.java
    Refactor variable names for area calculations and improve formatting.

--- q9.java
+++ q9.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class q9 {
     public static void main(String[] args) {
         Locale.setDefault(new Locale("pt", "BR"));
-        Scanner sc = new Scanner(System.in);
-        System.out.print("digite a medida A: ");
-        double a = sc.nextDouble();
-        System.out.print("digite a medida B: ");
-        double b = sc.nextDouble();
-        System.out.print("digite a medida C: ");
-        double c = sc.nextDouble();
-        double areaQuadrado = a*a;
-        double areaTriangulo = (a*b)/2.0;
-        double areaTrapezio = ((a+b)*c)/2.0;
-        System.out.printf("quadrado= %.4f%n", areaQuadrado);
-        System.out.printf("triangulo = %.4f%n", areaTriangulo);
-        System.out.printf("trapézio= %.4f%n", areaTrapezio);
+        Scanner sc=new Scanner(System.in);
+        System.out.print("digite a medida A:");
+        double a=sc.nextDouble();
+        System.out.print("digite a medida B:");
+        double b=sc.nextDouble();
+        System.out.print("digite a medida C:");
+        double c=sc.nextDouble();
+        double areaq=a*a;
+        double areat=(a*b)/2.0;
+        double areaTr=((a+b)*c)/2.0;
+        System.out.printf("quadrado= %.4f%n", areaq);
+        System.out.printf("triangulo = %.4f%n", areat);
+        System.out.printf("trapézio= %.4f%n", areatr);
         sc.close();
     }
 }
