6eec019b8947ef1737f6c2654807d5942f3ae32e
amandasguedes <amanda.guedes@aluno.ce.gov.br>
2026-05-09 21:30:35 -0300

    Update q12.java

--- q12.java
+++ q12.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class q12 {
     public static void main(String[] args) {
         Locale.setDefault(new Locale("pt", "BR"));
-        Scanner sc= new Scanner(System.in);
-        System.out.print("coeficiente a: ");
+        Scanner sc=new Scanner(System.in);
+        System.out.print("coeficiente a:");
         double a=sc.nextDouble();
-        System.out.print("coeficiente b: ");
+        System.out.print("coeficiente b:");
         double b=sc.nextDouble();
-        System.out.print("coeficiente c: ");
+        System.out.print("coeficiente c:");
         double c=sc.nextDouble();
-        double delta=Math.pow(b, 2) - (4 * a * c);
-        if (a == 0 || delta < 0) {
-            System.out.println("Esta equacao nao possui raizes reais");
+        double delta=Math.pow(b, 2) - (4*a*c);
+        if (a==0 || delta<0) {
+            System.out.println("esta equacao nao possui raizes reais");
         } else {
-            double x1 = (-b + Math.sqrt(delta)) / (2 * a);
-            double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+            double x1=(-b + Math.sqrt(delta))/(2 * a);
+            double x2=(-b - Math.sqrt(delta))/(2 * a);
             System.out.printf("X1 = %.4f%n", x1);
             System.out.printf("X2 = %.4f%n", x2);
         }
