df47081bade23057755622a0c8d4e681e48fe8d7
amandasguedes <amanda.guedes@aluno.ce.gov.br>
2026-05-09 21:27:55 -0300

    Update q6.java

--- q6.java
+++ q6.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class q6 {
     public static void main(String[] args) {
         Locale.setDefault(new Locale("pt", "BR"));
-        Scanner sc = new Scanner(System.in);
-        System.out.print("digite o valor do raio: ");
+        Scanner sc=new Scanner(System.in);
+        System.out.print("digite o valor do raio:");
         double r=sc.nextDouble();
-        double area = Math.PI * Math.pow(r, 2);
+        double area=Math.PI * Math.pow(r, 2);
         System.out.printf("area= %.3f%n", area);
         sc.close();
     }
