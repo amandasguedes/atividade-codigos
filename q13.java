5abb603c424bac47b05d7147bb802039205415a3
amandasguedes <amanda.guedes@aluno.ce.gov.br>
2026-05-09 21:31:36 -0300

    Update q13.java

--- q13.java
+++ q13.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 public class q13 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("primeiro valor: ");
-        int a = sc.nextInt();
-        System.out.print("segundo valor: ");
-        int b = sc.nextInt();
-        System.out.print("terceiro valor: ");
-        int c = sc.nextInt();
+        Scanner sc=new Scanner(System.in);
+        System.out.print("primeiro valor:");
+        int a=sc.nextInt();
+        System.out.print("segundo valor:");
+        int b=sc.nextInt();
+        System.out.print("terceiro valor:");
+        int c=sc.nextInt();
         int menor;
-        if (a <= b && a <= c) {
-            menor = a;
-        } else if (b <= c) {
-            menor = b;
+        if (a<=b && a<=c) {
+            menor=a;
+        } else if (b<=c) {
+            menor=b;
         } else {
-            menor = c;
+            menor=c;
         }
-        System.out.println("menor= " + menor);
+        System.out.println("menor="+menor);
         sc.close();
     }
 }
