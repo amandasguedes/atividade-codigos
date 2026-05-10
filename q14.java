f8d2452f92f58c6af0b99f742b2d6ceb2408a48d
amandasguedes <amanda.guedes@aluno.ce.gov.br>
2026-05-09 21:32:20 -0300

    Update q14.java

--- q14.java
+++ q14.java
@@ -1,16 +1,14 @@
 import java.util.Locale;
 import java.util.Scanner;
-
 public class q14 {
-
     public static void main(String[] args) {
         Locale.setDefault(new Locale("pt", "BR"));
-        Scanner sc = new Scanner(System.in);
-        System.out.print("quantidade de minutos: ");
+        Scanner sc=new Scanner(System.in);
+        System.out.print("quantidade de minutos:");
         int minutos = sc.nextInt();
         double valorPago = 50.0;
-        if (minutos > 100) {
-            valorp += (minutos - 100) * 2.0;
+        if (minutos>100) {
+            valorp+=(minutos-100)*2.0;
         }
         System.out.printf("valor a pagar: R$ %.2f%n", valorp);
         sc.close();
